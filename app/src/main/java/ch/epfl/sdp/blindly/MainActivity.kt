7d8d50dddc3892a012a39353d58573a4df5e4764
Tibor Vaughan <tibor.vaughan@epfl.ch>
2021-03-06 01:21:51 +0100

    add simple account activity

--- app/src/main/java/ch/epfl/sdp/blindly/MainActivity.kt
+++ app/src/main/java/ch/epfl/sdp/blindly/MainActivity.kt
@@ -1,8 +1,14 @@
 package ch.epfl.sdp.blindly
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import ch.epfl.sdp.blindly.R.id.button
+import ch.epfl.sdp.blindly.utils.UserHelper
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 
 
@@ -11,11 +17,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val auth = FirebaseAuth.getInstance()
-        if (auth.currentUser == null)
-            startActivity(Intent(this, SignInActivity::class.java))
-        else
-            println("111111")
-            println(auth.currentUser?.email)
+        setupButton()
+
+    }
+
+    private fun setupButton() {
+        val button = findViewById<Button>(button)
+        if (UserHelper.isLoggedIn()) {
+            button.setText(R.string.home_button_user_loggedin)
+            button.setOnClickListener { startActivity(Intent(this, MyAccountActivity::class.java)) }
+        } else {
+            button.setText(R.string.home_button_user_loggedout)
+            button.setOnClickListener { UserHelper.startSignInActivity(this) }
+        }
+    }
+
+
+    // [START auth_fui_result]
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == UserHelper.RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                setupButton()
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+                if (response != null) {
+                    Toast.makeText(applicationContext, getString(R.string.login_err, response.error?.errorCode ?: -1), Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 }
--- app/src/main/java/ch/epfl/sdp/blindly/utils/UserHelper.kt
+++ app/src/main/java/ch/epfl/sdp/blindly/utils/UserHelper.kt
@@ -1,8 +1,13 @@
 package ch.epfl.sdp.blindly.utils
 
+import android.Manifest
 import android.app.Activity
 import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.pm.PackageManager
+import android.telephony.TelephonyManager
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import androidx.core.app.ActivityCompat.startActivityForResult
 import ch.epfl.sdp.blindly.R
 import com.firebase.ui.auth.AuthUI
@@ -14,58 +19,73 @@ import com.google.firebase.ktx.Firebase
 
 class UserHelper {
 
-    private val providers = arrayListOf(
-        AuthUI.IdpConfig.EmailBuilder().setRequireName(false).build(),
-        //AuthUI.IdpConfig.PhoneBuilder().build(),
-        AuthUI.IdpConfig.GoogleBuilder().build()
-    )
-
-    public fun startSignInActivity(activity: Activity) {
-        startActivityForResult(
-            activity,
-            AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                //.setLogo(R.drawable.my_great_logo) // Set logo drawable
-                .setTheme(R.style.Theme_Blindly) // Set theme
-                /*.setTosAndPrivacyPolicyUrls(
-                "https://example.com/terms.html",
-                "https://example.com/privacy.html")*/
-                .build(),
-            UserHelper.RC_SIGN_IN, null
-        )
-    }
-
-    public fun signOut(activity: Activity, onComplete: OnCompleteListener<Void>) {
-        AuthUI.getInstance()
-            .signOut(activity)
-            .addOnCompleteListener(onComplete);
-    }
-
-    public fun delete(activity: Activity, onComplete: OnCompleteListener<Void>) {
-        AuthUI.getInstance()
-            .delete(activity)
-            .addOnCompleteListener(onComplete)
-    }
-
-    private fun getUserId(): String {
-        return FirebaseAuth.getInstance().currentUser.uid
-    }
-
-    private fun getMeta() {
-        val db = Firebase.firestore
-        db.collection("usersMeta").document(getUserId())
-            .get()
-            .addOnSuccessListener { document ->
-                    Log.d(TAG, "${document.id} => ${document.data}")
-            }
-            .addOnFailureListener { exception ->
-                Log.w(TAG, "Error getting documents.", exception)
-            }
-    }
-
     companion object {
 
-        private const val RC_SIGN_IN = 123
+        public fun startSignInActivity(activity: Activity) {
+            // Optionnaly get phone number to set default in login form
+            val phoneProvider = AuthUI.IdpConfig.PhoneBuilder()
+            /*
+            if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_PHONE_NUMBERS) == PackageManager.PERMISSION_GRANTED ){
+                var tMgr: TelephonyManager = activity.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+                phoneProvider.setDefaultNumber(tMgr.line1Number)
+            }*/
+            val providers = arrayListOf(
+                    AuthUI.IdpConfig.EmailBuilder().setRequireName(false).build(),
+                    phoneProvider.build(),
+                    AuthUI.IdpConfig.GoogleBuilder().build()
+            )
+            startActivityForResult(
+                    activity,
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            //.setLogo(R.drawable.my_great_logo) // Set logo drawable
+                            .setTheme(R.style.Theme_Blindly) // Set theme
+                            /*.setTosAndPrivacyPolicyUrls(
+                    "https://example.com/terms.html",
+                    "https://example.com/privacy.html")*/
+                            .build(),
+                    UserHelper.RC_SIGN_IN, null
+            )
+        }
+
+        public fun signOut(activity: Activity, onComplete: OnCompleteListener<Void>) {
+            AuthUI.getInstance()
+                    .signOut(activity)
+                    .addOnCompleteListener(onComplete);
+        }
+
+        public fun delete(activity: Activity, onComplete: OnCompleteListener<Void>) {
+            AuthUI.getInstance()
+                    .delete(activity)
+                    .addOnCompleteListener(onComplete)
+        }
+
+        public fun isLoggedIn(): Boolean {
+            return FirebaseAuth.getInstance().currentUser != null
+        }
+
+        public fun getEmail(): String {
+            return FirebaseAuth.getInstance().currentUser.email
+        }
+
+        private fun getUserId(): String {
+            return FirebaseAuth.getInstance().currentUser.uid
+        }
+
+        private fun getMeta() {
+            val db = Firebase.firestore
+            db.collection("usersMeta").document(getUserId())
+                    .get()
+                    .addOnSuccessListener { document ->
+                        Log.d(TAG, "${document.id} => ${document.data}")
+                    }
+                    .addOnFailureListener { exception ->
+                        Log.w(TAG, "Error getting documents.", exception)
+                    }
+        }
+
+
+        public const val RC_SIGN_IN = 123
     }
 }
