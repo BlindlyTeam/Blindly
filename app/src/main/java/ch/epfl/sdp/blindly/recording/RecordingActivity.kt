ec1f294662f15732bc315641e566edcc31e4fe7d
Kilian Schneiter <kilian.schneiter@epfl.ch>
2021-03-07 17:22:56 +0100

    Add empty audio recording activity

---
+++ app/src/main/java/ch/epfl/sdp/blindly/recording/RecordingActivity.kt
@@ -0,0 +1,12 @@
+package ch.epfl.sdp.blindly.recording
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ch.epfl.sdp.blindly.R
+
+class RecordingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recording)
+    }
+}
