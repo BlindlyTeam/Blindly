5fa4aef81029f28245aa395065c7865e58765736
Cagin Tanir <cagin.tanir@epfl.ch>
2021-03-09 21:15:25 +0100

    gender page with specifiable option added

--- app/src/main/java/ch/epfl/sdp/blindly/Profile4.kt
+++ app/src/main/java/ch/epfl/sdp/blindly/Profile4.kt
@@ -1,11 +1,27 @@
 package ch.epfl.sdp.blindly
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Profile4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.set_profile_4)
     }
+
+    fun start_profile_5(view: View) {
+        // Do something in response to button
+        val intent = Intent(this, Profile5::class.java)
+        startActivity(intent)
+    }
+
+    fun start_profile_4_2(view: View) {
+        // Do something in response to button
+        val intent = Intent(this, Profile4_2::class.java)
+        startActivity(intent)
+    }
+
+
 }
---
+++ app/src/main/java/ch/epfl/sdp/blindly/Profile5.kt
@@ -0,0 +1,11 @@
+package ch.epfl.sdp.blindly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Profile5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.set_profile_5)
+    }
+}
