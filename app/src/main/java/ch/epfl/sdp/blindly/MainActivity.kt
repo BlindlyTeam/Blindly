2e1b67cfbab828087c9a4231deba98ff4cbca0c9
Kilian Schneiter <kilian.schneiter@bluewin.ch>
2021-03-11 21:59:46 +0100

    Fixed issues for merging

--- app/src/main/java/ch/epfl/sdp/blindly/MainActivity.kt
+++ app/src/main/java/ch/epfl/sdp/blindly/MainActivity.kt
@@ -1,19 +1,11 @@
 package ch.epfl.sdp.blindly
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import ch.epfl.sdp.blindly.recording.RecordingActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    fun launchRecording(view: View) {
-        val intent = Intent(this, RecordingActivity::class.java)
-        startActivity(intent)
     }
 }
