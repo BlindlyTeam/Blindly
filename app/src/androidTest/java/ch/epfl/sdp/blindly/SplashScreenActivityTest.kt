8f67fc7c1d4675114f88bef4945d368aa8fe1936
Capucine S <33542708+cmsigrist@users.noreply.github.com>
2021-03-11 15:10:41 +0100

    Implement Splash_Screen, no testsd

---
+++ app/src/androidTest/java/ch/epfl/sdp/blindly/SplashScreenActivityTest.kt
@@ -0,0 +1,25 @@
+package ch.epfl.sdp.blindly
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.intent.Intents.init
+import androidx.test.espresso.intent.Intents.release
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class SplashScreenActivityTest {
+    @get:Rule
+    val activityRule = ActivityScenarioRule(SplashScreen::class.java)
+    @Test
+    fun mainActivityAppearsAfterDelay(){
+        init()
+        onView(withId(R.id.splash_screen_bckg))
+        release()
+    }
+
+}
--- app/src/main/java/ch/epfl/sdp/blindly/SplashScreen.kt
+++ app/src/main/java/ch/epfl/sdp/blindly/SplashScreen.kt
@@ -1,29 +1,28 @@
 package ch.epfl.sdp.blindly
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
+val SPLASH_SCREEN_DELAY : Long = 2500
 class SplashScreen : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
         supportActionBar?.hide()
-
         window.decorView.apply {
-            // Hide both the navigation bar and the status bar.
-            // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-            // a general rule, you should design your app to hide the status bar whenever you
-            // hide the navigation bar.
             systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
-        }
+            }
 
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this@SplashScreen, MainActivity::class.java))
             finish()
-        },2500)
+        }, SPLASH_SCREEN_DELAY)
     }
 }
