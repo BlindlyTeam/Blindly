2dab8d3896dff8b23344545c8f5e9f4c157334a6
Cagin Tanir <cagin.tanir@epfl.ch>
2021-03-11 19:20:49 +0100

    First tests added for UI-profile

---
+++ app/src/androidTest/java/ch/epfl/sdp/blindly/TestProfile2.kt
@@ -0,0 +1,35 @@
+package ch.epfl.sdp.blindly
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matchers
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class TestProfile2 {
+
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(Profile2::class.java)
+
+
+    @Test
+    fun testProfile2FiresProfile3() {
+        Intents.init()
+        val buttonStart = Espresso.onView(withId(R.id.button_p2))
+        buttonStart.perform(click())
+        intended(hasComponent(Profile3::class.java.name))
+        Intents.release()
+    }
+
+}
