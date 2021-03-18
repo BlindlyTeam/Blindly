8a5ee640b0df8812d514da89900289cc313a7322
Cagin Tanir <cagin.tanir@epfl.ch>
2021-03-19 00:18:09 +0100

    Conflicts resolved

--- app/src/androidTest/java/ch/epfl/sdp/blindly/TestMainActivity.kt
+++ app/src/androidTest/java/ch/epfl/sdp/blindly/TestMainActivity.kt
@@ -8,10 +8,7 @@ import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-<<<<<<< HEAD
 import ch.epfl.sdp.blindly.Profile.Profile1
-=======
->>>>>>> main
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
