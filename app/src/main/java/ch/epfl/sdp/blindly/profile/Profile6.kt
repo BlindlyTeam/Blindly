6709a0b14100874268625a9754bbaa0ff27148d7
Cagin Tanir <cagin.tanir@epfl.ch>
2021-03-19 05:30:02 +0100

    Show me ... page check and error message added

--- app/src/main/java/ch/epfl/sdp/blindly/profile/Profile6.kt
+++ app/src/main/java/ch/epfl/sdp/blindly/profile/Profile6.kt
@@ -3,6 +3,8 @@ package ch.epfl.sdp.blindly.profile
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import ch.epfl.sdp.blindly.R
 
@@ -12,8 +14,17 @@ class Profile6 : AppCompatActivity() {
         setContentView(R.layout.set_profile_6)
     }
 
-    fun start_profile_7(view: View) {
-        val intent = Intent(this, Profile7::class.java)
-        startActivity(intent)
+    fun startProfile7(view: View) {
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup_p6)
+        findViewById<TextView>(R.id.warning_p6).visibility = View.INVISIBLE
+
+        //nothing is checked
+        if (radioGroup.checkedRadioButtonId == -1) {
+            findViewById<TextView>(R.id.warning_p6).visibility = View.VISIBLE
+        } else {
+            val intent = Intent(this, Profile7::class.java)
+            startActivity(intent)
+        }
     }
+
 }
