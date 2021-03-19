94d66115b6afedef281dbe83de23f723a152da5f
Cagin Tanir <cagin.tanir@epfl.ch>
2021-03-19 06:42:18 +0100

    Chip group check added, not complete

--- app/src/main/java/ch/epfl/sdp/blindly/profile/Profile5.kt
+++ app/src/main/java/ch/epfl/sdp/blindly/profile/Profile5.kt
@@ -6,14 +6,45 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import ch.epfl.sdp.blindly.R
 
+
 class Profile5 : AppCompatActivity() {
+
+    private val limitSelection = 3
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.set_profile_5)
     }
 
-    fun start_profile_6(view: View) {
+    fun startProfile6(view: View) {
         val intent = Intent(this, Profile6::class.java)
         startActivity(intent)
     }
+
+//    fun startProfile6(view: View) {
+//        findViewById<TextView>(R.id.warning_p5_1).visibility = View.INVISIBLE
+//        findViewById<TextView>(R.id.warning_p5_2).visibility = View.INVISIBLE
+//
+//        val chipGroup = findViewById<ChipGroup>(R.id.chipGroup_p5)
+//        var ids = chipGroup.checkedChipIds
+//        val size = ids.size
+//
+//        when {
+//            //none selected
+//            size < 1 -> {
+//                findViewById<TextView>(R.id.guideline_p5).visibility = View.INVISIBLE
+//                findViewById<TextView>(R.id.warning_p5_1).visibility = View.VISIBLE
+//            }
+//            size > limitSelection -> {
+//                findViewById<TextView>(R.id.guideline_p5).visibility = View.INVISIBLE
+//                findViewById<TextView>(R.id.warning_p5_2).visibility = View.VISIBLE
+//            }
+//            //correct numbers of selection
+//            else -> {
+//                val intent = Intent(this, Profile6::class.java)
+//                startActivity(intent)
+//            }
+//        }
+//
+//    }
 }
