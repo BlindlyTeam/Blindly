fa1a4f28b1883430a20f08385be2c906f4888ab0
Capucine S <33542708+cmsigrist@users.noreply.github.com>
2021-03-24 12:15:04 +0100

    Create User class to set the preferences of the user

---
+++ app/src/main/java/ch/epfl/sdp/blindly/SignUp.kt
@@ -0,0 +1,55 @@
+package ch.epfl.sdp.blindly
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import ch.epfl.sdp.blindly.profile.Profile1
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class SignUp : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        auth = Firebase.auth
+
+        val emailAddress = findViewById<EditText>(R.id.editTextEmailAddress).text.toString()
+        val password = findViewById<EditText>(R.id.editTextPassword).text.toString()
+
+        findViewById<Button>(R.id.create_account_button).setOnClickListener {
+            createAccount(emailAddress, password)
+        }
+
+    }
+
+    private fun createAccount(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val intent = Intent(this, Profile1::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+                    }
+                }
+    }
+
+    companion object {
+        private const val TAG = "EmailPassword"
+    }
+}
---
+++ app/src/main/java/ch/epfl/sdp/blindly/User.kt
@@ -0,0 +1,92 @@
+package ch.epfl.sdp.blindly
+
+import android.util.Log
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.ktx.userProfileChangeRequest
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class User {
+    //Set User's information after set_profile
+    private fun setUserProfile(name: String, location: String, birthday: String, genre: String,
+                               sexualOrientations: List<String>, showMe: String,
+                               passions: List<String>) {
+
+        val database = Firebase.database
+        val user = Firebase.auth.currentUser
+
+        if(user != null) {
+            updateName(name)
+            val newUser = mapOf("name" to name,
+                    "email" to user.email,
+                    "location" to location,
+                    "birthday" to birthday,
+                    "genre" to genre,
+                    "sexual_orientation" to sexualOrientations,
+                    "show_me" to showMe,
+                    "passions" to passions)
+
+            database.reference.child(USER_PATH).child(user.uid).setValue(newUser)
+        }
+    }
+
+    private fun updateName(name: String) {
+        val user = Firebase.auth.currentUser
+
+        val profileUpdates = userProfileChangeRequest {
+            displayName = name
+        }
+
+        user!!.updateProfile(profileUpdates)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, "User profile updated.")
+                    }
+                }
+    }
+
+    private fun updateEmail(email: String) {
+        val user = Firebase.auth.currentUser
+
+        user!!.updateEmail(email)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, "User email address updated.")
+                    }
+                }
+    }
+
+    private fun updatePassword(password: String) {
+        val user = Firebase.auth.currentUser
+
+        user!!.updatePassword(password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, "User password updated.")
+                    }
+                }
+    }
+
+    /**
+     * path: the path of the value to change inside the database
+     * newValue: should either be a String or a List<String>
+     */
+    private fun <T>updateProfile(path: String, newValue: T) {
+        val database = Firebase.database
+        val user = Firebase.auth.currentUser
+        if(newValue !is String || newValue !is List<*>) {
+            throw IllegalArgumentException("newValue must be a String or a List of Strings")
+        }
+
+        if(user != null)
+            database.reference.child(USER_PATH)
+                    .child(user.uid)
+                    .child(path)
+                    .setValue(newValue)
+    }
+
+    companion object {
+        private const val TAG = "User"
+        private const val USER_PATH: String = "users"
+    }
+}
