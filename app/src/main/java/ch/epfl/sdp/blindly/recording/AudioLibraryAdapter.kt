0532cabcd3f4c7440dddeccdbd04d21f7f1c1538
Kilian Schneiter <kilian.schneiter@bluewin.ch>
2021-04-14 16:56:16 +0200

    Add kdoc in adapter

--- app/src/main/java/ch/epfl/sdp/blindly/recording/AudioLibraryAdapter.kt
+++ app/src/main/java/ch/epfl/sdp/blindly/recording/AudioLibraryAdapter.kt
@@ -23,21 +23,32 @@ import java.io.File
 private const val PRESENTATION_AUDIO_NAME = "PresentationAudio.3gp"
 
 /**
- * Serves as an adapter to add audio recordings in a RecyclerView
+ * Adapter to use a RecyclerView as an audio library.
+ *
+ * @property recordList list of AudioRecords currently in the adapter
+ * @property viewHolderList list of ViewHolders corresponding to each record
+ * @property context context of the RecyclerView
+ * @property listener handles clicks on items
  */
 class AudioLibraryAdapter(var recordList: ArrayList<AudioRecord>,
                           var viewHolderList: ArrayList<ViewHolder>,
                           var context: Context,
                           private val listener: OnItemClickListener)
     : RecyclerView.Adapter<AudioLibraryAdapter.ViewHolder>() {
-    /**
-     * Provide a reference to the type of views that you are using
-     * (custom ViewHolder).
-     */
     var mediaPlayer: MediaPlayer? = null
     private var isPlayerPaused = false
     private var isPlayerStopped = true
 
+    /**
+     * Custom ViewHolder class that contains all the elements that will be used later on in
+     * [onBindViewHolder].
+     *
+     * @constructor
+     * Sets the click listener for the layout that handles the layout expand click (the one with
+     * the name and duration), and sets the visibility of the expandable layout to gone.
+     *
+     * @param view the current view
+     */
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
         val recordName: TextView = view.findViewById(R.id.recordName)
         val recordDuration: TextView = view.findViewById(R.id.recordDuration)
@@ -50,21 +61,25 @@ class AudioLibraryAdapter(var recordList: ArrayList<AudioRecord>,
         val selectButton: Button = view.findViewById(R.id.selectButton)
 
         init {
-            // Define click listener for the ViewHolder's View.
             nameDurationLayout.setOnClickListener(this)
             expandableLayout.visibility = View.GONE
         }
 
         override fun onClick(v: View?) {
-            // Forward the click to the main activity
             if (adapterPosition != RecyclerView.NO_POSITION) {
                 listener.onItemClick(adapterPosition)
             }
         }
     }
 
-    // Expands or collapse the layout for playing the audio file, and collapse the others
-    private fun toggleLayout(isExpanded: Boolean, v: View, layoutExpand: RelativeLayout) {
+    /**
+     * Expands or collapse the layout of the audio file, and collapse other ones.
+     *
+     * @param isExpanded if the record is currently expanded in layout
+     * @param view the current view
+     * @param layoutExpand the layout to expand/collapse
+     */
+    private fun toggleLayout(isExpanded: Boolean, view: View, layoutExpand: RelativeLayout) {
         for (i in 0 until recordList.size) {
             if (recordList[i].isExpanded) {
                 RecordAnimations.collapse(viewHolderList[i].expandableLayout)
@@ -78,19 +93,35 @@ class AudioLibraryAdapter(var recordList: ArrayList<AudioRecord>,
         }
     }
 
-    // Create new views (invoked by the layout manager)
+    /**
+     * Create new views for each item of the list.
+     * This function is invoked by the layout manager.
+     *
+     * @param viewGroup the group where the ViewHolder belongs
+     * @param viewType the type of view
+     * @return a new view, which defines the UI of the list items
+     */
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        // Create a new view, which defines the UI of the list item
         val view = LayoutInflater.from(context).inflate(R.layout.audio_recording, null)
         viewHolderList.add(ViewHolder(view))
         return ViewHolder(view)
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
+    /**
+     * Binds all the content to the view, and handles click listeners.
+     * This function is invoked by the layout manager.
+     *
+     * @param viewHolder the ViewHolder of the current item
+     * @param position the position of the item in the RecyclerView
+     */
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        // Get element from your dataset at this position and replace the
-        // contents of the view with that element
+        val playBar = viewHolder.playBar
+        val playTimer = viewHolder.playTimer
+        val remainingTimer = viewHolder.remainingTimer
+        val playPauseButton = viewHolder.playPauseButton
+        val movePlayBarThread = createPlayBarThread(playBar)
+
         viewHolder.recordName.text = recordList[position].name
         viewHolder.recordDuration.text = recordList[position].durationText
         viewHolder.nameDurationLayout.setOnClickListener {
@@ -98,12 +129,6 @@ class AudioLibraryAdapter(var recordList: ArrayList<AudioRecord>,
             toggleLayout(notIsExpanded, it, viewHolder.expandableLayout)
             recordList[position].isExpanded = notIsExpanded
         }
-
-        val playBar = viewHolder.playBar
-        val playTimer = viewHolder.playTimer
-        val remainingTimer = viewHolder.remainingTimer
-        val playPauseButton = viewHolder.playPauseButton
-        val movePlayBarThread = createPlayBarThread(playBar)
 
         setCountDownTimer(remainingTimer)
 
@@ -206,11 +231,10 @@ class AudioLibraryAdapter(var recordList: ArrayList<AudioRecord>,
         playPauseButton.setImageResource(android.R.drawable.ic_media_pause)
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
     override fun getItemCount() = recordList.size
 
     /**
-     * Used to handle clicks in the activity
+     * Handles clicks on different items
      */
     interface OnItemClickListener {
         fun onItemClick(position: Int)
