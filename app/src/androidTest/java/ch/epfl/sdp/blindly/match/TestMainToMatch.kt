dcc3feb44944245a3f52fd7a1791151d3b01847b
r-ismer <61227308+r-ismer@users.noreply.github.com>
2021-04-19 01:15:11 +0200

    Update app/src/androidTest/java/ch/epfl/sdp/blindly/match/TestMainToMatch.kt
    Co-authored-by: Lollipoke <melissa.gehring@epfl.ch>

--- app/src/androidTest/java/ch/epfl/sdp/blindly/match/TestMainToMatch.kt
+++ app/src/androidTest/java/ch/epfl/sdp/blindly/match/TestMainToMatch.kt
@@ -29,7 +29,7 @@ class TestMainToMatch {
     }
 
     @Test
-    fun onButtonClickLunchMatchActivity() {
+    fun onButtonClickLaunchMatchActivity() {
         init()
         onView(withId(R.id.button_to_match)).perform(click())
         intended(hasComponent(MatchActivity::class.java.name))
