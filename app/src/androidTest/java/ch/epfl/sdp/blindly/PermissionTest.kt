4d68524e8abe248d83d497dc784dc8fb55708904
Tibor Vaughan <tibor.vaughan@epfl.ch>
2021-04-22 18:43:53 +0200

    add rule

--- app/src/androidTest/java/ch/epfl/sdp/blindly/PermissionTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/blindly/PermissionTest.kt
@@ -9,6 +9,7 @@ import androidx.test.espresso.intent.Intents.release
 import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.rule.GrantPermissionRule
 import ch.epfl.sdp.blindly.profile_setup.ProfileName
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -25,6 +26,12 @@ class PermissionTest {
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
+    @Rule
+    @JvmField
+    val grantPermissionRule: GrantPermissionRule = GrantPermissionRule
+        .grant(android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            android.Manifest.permission.ACCESS_FINE_LOCATION)
+
     @Test
     fun testLocationPermissionFiresProfileName() {
         init()
@@ -32,9 +39,9 @@ class PermissionTest {
         val buttonContinue = Espresso.onView(ViewMatchers.withId(R.id.button))
         buttonContinue.perform(ViewActions.click())
 
-        activityRule.scenario.onActivity { activity ->
+        /*activityRule.scenario.onActivity { activity ->
             activity.sendBroadcast(Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS))
-        }
+        }*/
         Thread.sleep(500)
         Intents.intended(IntentMatchers.hasComponent(ProfileName::class.java.name))
         release()
