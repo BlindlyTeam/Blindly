060dd38d58e7d77125a9f37e07b81d6ff5288a03
Kilian Schneiter <kilian.schneiter@bluewin.ch>
2021-04-28 13:48:02 +0200

    Resolve conflict

--- app/src/androidTest/java/ch/epfl/sdp/blindly/fake_module/FakeUserCacheModule.kt
+++ app/src/androidTest/java/ch/epfl/sdp/blindly/fake_module/FakeUserCacheModule.kt
@@ -30,19 +30,6 @@ open class FakeUserCacheModule {
         private const val description = "Student"
         private val ageRange = listOf(30, 50)
         val fakeUser = User.Builder()
-<<<<<<< HEAD
-            .setUsername(username)
-            .setLocation(location)
-            .setBirthday(birthday)
-            .setGender(gender)
-            .setSexualOrientations(sexual_orientations)
-            .setShowMe(show_me)
-            .setPassions(passions)
-            .setRadius(radius)
-            .setDescription(description)
-            .setMatches(matches)
-            .build()
-=======
                 .setUsername(username)
                 .setLocation(location)
                 .setBirthday(birthday)
@@ -55,7 +42,6 @@ open class FakeUserCacheModule {
                 .setMatches(matches)
                 .setAgeRange(ageRange)
                 .build()
->>>>>>> f05c67b9737a77d140e728bd037118c202b33f16
     }
 
     @Singleton
