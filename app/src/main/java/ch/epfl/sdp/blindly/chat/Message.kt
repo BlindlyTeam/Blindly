a38807187917cc37832de263e5ab4bfd56b7962c
Cagin Tanir <cagin.tanir@epfl.ch>
2021-04-29 11:50:10 +0200

    Remove irrelevant comment

--- app/src/main/java/ch/epfl/sdp/blindly/chat/Message.kt
+++ app/src/main/java/ch/epfl/sdp/blindly/chat/Message.kt
@@ -9,8 +9,6 @@ class Message {
 
     // timestamp is to achieve correct ordering of the messages and ensure fetching is in
     // same ordering the next time users open the chat
-    // Note: I considered usage from different timezones however since matches are only
-    // allowed to be within a certain distance, there won't be any problem.
     var timestamp: Long? = null
     var messageText: String? = null
     var currentUserId: String? = null
