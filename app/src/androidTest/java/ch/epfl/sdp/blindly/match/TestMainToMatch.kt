976754d7736ecfd1e148fb604d2b5d79089bbd5f
Mélissa <melissa.gehring@epfl.ch>
2021-04-30 09:32:01 +0200

    Improve tests

--- app/src/androidTest/java/ch/epfl/sdp/blindly/match/TestMainToMatch.kt
+++ app/src/androidTest/java/ch/epfl/sdp/blindly/match/TestMainToMatch.kt
@@ -4,7 +4,8 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.intent.Intents.*
+import androidx.test.espresso.intent.Intents.init
+import androidx.test.espresso.intent.Intents.release
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
