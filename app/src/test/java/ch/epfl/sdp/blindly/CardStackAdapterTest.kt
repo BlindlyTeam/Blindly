a0ec2093ffac070d20a39b98cf46d4346895ce21
Mélissa <melissa.gehring@epfl.ch>
2021-05-07 10:10:08 +0200

    Add another test

---
+++ app/src/test/java/ch/epfl/sdp/blindly/CardStackAdapterTest.kt
@@ -0,0 +1,18 @@
+package ch.epfl.sdp.blindly;
+
+import ch.epfl.sdp.blindly.match.CardStackAdapter
+import ch.epfl.sdp.blindly.match.Profile
+import com.google.firebase.storage.FirebaseStorage
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
+import org.junit.Test;
+
+class CardStackAdapterTest {
+    private val PROFILES = listOf(Profile("Richard", 25, "Bonjour c'est Richard", "Wine","Path"))
+    @Test
+    fun constructorInitializesProfilesCorrectly(){
+        val cardStackAdapter = CardStackAdapter(PROFILES, FirebaseStorage.getInstance())
+        assertThat(cardStackAdapter.itemCount, equalTo(PROFILES.size))
+    }
+
+}
