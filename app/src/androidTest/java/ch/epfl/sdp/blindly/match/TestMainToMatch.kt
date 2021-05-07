3d55cfcabdffe06ad47d4774e9eb2b03fa28b754
Mélissa <melissa.gehring@epfl.ch>
2021-05-07 09:19:30 +0200

    Add test

--- app/src/androidTest/java/ch/epfl/sdp/blindly/match/TestMainToMatch.kt
+++ app/src/androidTest/java/ch/epfl/sdp/blindly/match/TestMainToMatch.kt
@@ -1,16 +1,20 @@
 package ch.epfl.sdp.blindly.match
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents.init
 import androidx.test.espresso.intent.Intents.release
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import ch.epfl.sdp.blindly.R
 import ch.epfl.sdp.blindly.main_screen.MainScreen
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
-
-const val SLEEP_TIME = 250L
+import org.junit.Test
 
 @HiltAndroidTest
 class TestMainToMatch {
@@ -29,6 +33,11 @@ class TestMainToMatch {
     @After
     fun afterEach() {
         release()
+    }
+
+    @Test
+    fun cardStackAdapterIsCreatedOnFragmentCreation(){
+        onView(withId(R.id.card_stack_view)).check(matches(isDisplayed()))
     }
 
     //Had to remove the tests while we look for a way to test the DB input
