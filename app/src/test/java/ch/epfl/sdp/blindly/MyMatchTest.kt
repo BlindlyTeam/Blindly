b0b82526130bbe57867a4b1bd258e84d6a75f970
Kilian Schneiter <kilian.schneiter@bluewin.ch>
2021-05-26 12:35:53 +0200

    Add tests for MyMatch data class

--- app/src/test/java/ch/epfl/sdp/blindly/MyMatchTest.kt
+++ app/src/test/java/ch/epfl/sdp/blindly/MyMatchTest.kt
@@ -5,30 +5,70 @@ import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 
+private const val NAME1 = "test_name1"
+private const val USER_ID1 = "user_id1"
+private const val IS_EXPANDED1 = false
+private const val NAME2 = "test_name2"
+private const val USER_ID2 = "user_id2"
+private const val IS_EXPANDED2 = true
 
 class MyMatchTest {
-    companion object {
-        private const val NAME = "test_name"
-        private const val USER_ID = "user_id"
-        private var IS_EXPANDED = false
-    }
+    private val myMatch1 = MyMatch(NAME1, USER_ID1, IS_EXPANDED1)
 
     @Test
     fun myMatchUidSetCorrectly() {
-        val myMatch = MyMatch(NAME, USER_ID, IS_EXPANDED)
-        assertThat(myMatch.uid, equalTo(USER_ID))
-
+        assertThat(myMatch1.uid, equalTo(USER_ID1))
     }
 
     @Test
     fun myMatchNameSetCorrectly() {
-        val myMatch = MyMatch(NAME, USER_ID, IS_EXPANDED)
-        assertThat(myMatch.name, equalTo(NAME))
+        assertThat(myMatch1.name, equalTo(NAME1))
     }
 
     @Test
     fun myMatchIsExpandedSetCorrectly() {
-        val myMatch = MyMatch(NAME, USER_ID, IS_EXPANDED)
-        assertThat(myMatch.isExpanded, equalTo(IS_EXPANDED))
+        assertThat(myMatch1.isExpanded, equalTo(IS_EXPANDED1))
+    }
+
+    @Test
+    fun equalsIsTrueForSameMyMatch() {
+        val myMatch2 = MyMatch(NAME1, USER_ID1, IS_EXPANDED1)
+        assertThat(myMatch1 == myMatch2, equalTo(true))
+    }
+
+    @Test
+    fun equalsIsFalseForDifferentNames() {
+        val myMatch2 = MyMatch(NAME2, USER_ID1, IS_EXPANDED1)
+        assertThat(myMatch1 == myMatch2, equalTo(false))
+    }
+
+    @Test
+    fun equalsIsFalseForDifferentIDs() {
+        val myMatch2 = MyMatch(NAME1, USER_ID2, IS_EXPANDED1)
+        assertThat(myMatch1 == myMatch2, equalTo(false))
+    }
+
+    @Test
+    fun equalsIsFalseForDifferentIsExpanded() {
+        val myMatch2 = MyMatch(NAME1, USER_ID1, IS_EXPANDED2)
+        assertThat(myMatch1 == myMatch2, equalTo(false))
+    }
+
+    @Test
+    fun hashCodeWorksWithSameObjects() {
+        val myMatch2 = MyMatch(NAME1, USER_ID1, IS_EXPANDED1)
+        assertThat(myMatch1.hashCode() == myMatch2.hashCode(), equalTo(true))
+    }
+
+    @Test
+    fun hashCodeWorksWithDifferentObjects() {
+        val myMatch2 = MyMatch(NAME2, USER_ID2, IS_EXPANDED2)
+        assertThat(myMatch1.hashCode() == myMatch2.hashCode(), equalTo(false))
+    }
+
+    @Test
+    fun copyWorks() {
+        val myMatch2 = myMatch1.copy()
+        assertThat(myMatch1 == myMatch2, equalTo(true))
     }
 }
