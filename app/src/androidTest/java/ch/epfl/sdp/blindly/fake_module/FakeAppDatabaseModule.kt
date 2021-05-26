ef7b68373e296e708a4ac3cb815563bf4e051a41
r-ismer <richard.ismer@gmail.com>
2021-05-26 20:43:47 +0200

    cleaned fake appdatabasemodule and added network check if using geocoder

--- app/src/androidTest/java/ch/epfl/sdp/blindly/fake_module/FakeAppDatabaseModule.kt
+++ app/src/androidTest/java/ch/epfl/sdp/blindly/fake_module/FakeAppDatabaseModule.kt
@@ -9,10 +9,9 @@ import dagger.Module
 import dagger.hilt.components.SingletonComponent
 import dagger.hilt.testing.TestInstallIn
 import ch.epfl.sdp.blindly.dependency_injection.AppDatabaseModule
-import ch.epfl.sdp.blindly.location.AndroidLocationService
-import ch.epfl.sdp.blindly.user.User
 import dagger.Provides
 import dagger.hilt.android.qualifiers.ApplicationContext
+import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito
 import javax.inject.Singleton
 
@@ -25,36 +24,7 @@ import javax.inject.Singleton
 
 open class FakeAppDatabaseModule {
     companion object {
-        private const val uid = "abcd1234"
-        private const val username = "Jane Doe"
-        private val location =
-            AndroidLocationService.createLocationTableEPFL() // Ecublens, Switzerland
-        private const val birthday = "01.01.01"
-        private const val gender = "Woman"
-        private val sexualOrientations = listOf("Asexual")
-        private const val showMe = "Everyone"
-        private val passions = listOf("Coffee", "Tea")
-        private const val radius = 150
-        private val matches: List<String> = listOf("a1", "b2")
-        private val likes: List<String> = listOf("c3", "d4")
-        private const val recordingPath =
-            "Recordings/OKj1UxZao3hIVtma95gWZlner9p1-PresentationAudio.amr"
-        private val ageRange = listOf(30, 50)
-        val fakeUser = User.Builder()
-            .setUid(uid)
-            .setUsername(username)
-            .setLocation(location)
-            .setBirthday(birthday)
-            .setGender(gender)
-            .setSexualOrientations(sexualOrientations)
-            .setShowMe(showMe)
-            .setPassions(passions)
-            .setRadius(radius)
-            .setMatches(matches)
-            .setLikes(likes)
-            .setRecordingPath(recordingPath)
-            .setAgeRange(ageRange)
-            .build()
+        val fakeUser = FakeUserCacheModule.fakeUser
     }
 
     @Singleton
@@ -67,23 +37,23 @@ open class FakeAppDatabaseModule {
     @Provides
     open fun provideUserDAO(database: AppDatabase): UserDAO {
         val userDAO = Mockito.mock(UserDAO::class.java)
-        Mockito.`when`(userDAO.getUser(uid)).thenReturn(fakeUser)
-        Mockito.`when`(userDAO.getUserUid(uid)).thenReturn(uid)
-        Mockito.`when`(userDAO.getUserName(uid)).thenReturn(username)
-        Mockito.`when`(userDAO.getUserLocation(uid)).thenReturn(location)
-        Mockito.`when`(userDAO.getUserBirthday(uid)).thenReturn(birthday)
-        Mockito.`when`(userDAO.getUserGender(uid)).thenReturn(gender)
-        Mockito.`when`(userDAO.getUserSexualOrientation(uid)).thenReturn(sexualOrientations)
-        Mockito.`when`(userDAO.getUserShowMe(uid)).thenReturn(showMe)
-        Mockito.`when`(userDAO.getUserPassions(uid)).thenReturn(passions)
-        Mockito.`when`(userDAO.getUserRadius(uid)).thenReturn(radius)
-        Mockito.`when`(userDAO.getUserMatches(uid)).thenReturn(matches)
-        Mockito.`when`(userDAO.getUserLikes(uid)).thenReturn(likes)
-        Mockito.`when`(userDAO.getUserRecordingPath(uid)).thenReturn(recordingPath)
-        Mockito.`when`(userDAO.getUserAgeRange(uid)).thenReturn(ageRange)
-        Mockito.`when`(userDAO.deleteUser(UserEntity(uid, fakeUser))).thenReturn(1)
-        Mockito.`when`(userDAO.getUserInfo(uid)).thenReturn(UserEntity(uid, fakeUser))
-        Mockito.`when`(userDAO.updateUser(UserEntity(uid, fakeUser))).thenReturn(1)
+        Mockito.`when`(userDAO.getUser(any())).thenReturn(fakeUser)
+        Mockito.`when`(userDAO.getUserUid(any())).thenReturn(fakeUser.uid)
+        Mockito.`when`(userDAO.getUserName(any())).thenReturn(fakeUser.username)
+        Mockito.`when`(userDAO.getUserLocation(any())).thenReturn(fakeUser.location)
+        Mockito.`when`(userDAO.getUserBirthday(any())).thenReturn(fakeUser.birthday)
+        Mockito.`when`(userDAO.getUserGender(any())).thenReturn(fakeUser.gender)
+        Mockito.`when`(userDAO.getUserSexualOrientation(any())).thenReturn(fakeUser.sexualOrientations)
+        Mockito.`when`(userDAO.getUserShowMe(any())).thenReturn(fakeUser.showMe)
+        Mockito.`when`(userDAO.getUserPassions(any())).thenReturn(fakeUser.passions)
+        Mockito.`when`(userDAO.getUserRadius(any())).thenReturn(fakeUser.radius)
+        Mockito.`when`(userDAO.getUserMatches(any())).thenReturn(fakeUser.matches)
+        Mockito.`when`(userDAO.getUserLikes(any())).thenReturn(fakeUser.likes)
+        Mockito.`when`(userDAO.getUserRecordingPath(any())).thenReturn(fakeUser.recordingPath)
+        Mockito.`when`(userDAO.getUserAgeRange(any())).thenReturn(fakeUser.ageRange)
+        Mockito.`when`(userDAO.deleteUser(UserEntity(any(), fakeUser))).thenReturn(1)
+        Mockito.`when`(userDAO.getUserInfo(any())).thenReturn(UserEntity(fakeUser.uid!!, fakeUser))
+        Mockito.`when`(userDAO.updateUser(UserEntity(any(), fakeUser))).thenReturn(1)
         return userDAO
     }
 
